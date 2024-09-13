324c6e9dac615be903fc6543a62d31cc6a3f5b02
Gagikhalvashi28 <xalvashigagi@gmail.com>
2024-09-13 19:08:16 +0400

    commit

---
+++ Level 17/Homework/Homeowrk01.py
@@ -0,0 +1,7 @@
+num1 = int(input("შეიყვანე რიცხვი"))
+num2 = int(input("შეიყვანე რიცხვი"))
+print(num1 + num2)
+print(num2 - num1)
+print(num2 * num1)
+print(num2 / num1)
+print(num2 ** num1)
---
+++ Level 17/Homework/Homewrok02.py
@@ -0,0 +1,7 @@
+num1 = int(input("შეიყვანე რიცხვი"))
+num2 = int(input("შეიყვანე რიცხვი"))
+
+
+jami  = num1 + num2
+sashualo = jami / 2
+print("საშუალო არითმეტიკულია" + " " + str(sashualo))
